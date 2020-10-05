7024e1f236d58f1987e72000169fcfc8c393d9d1
zahra <zahramousavidk@gmail.com>
2020-10-05 05:45:08 -0700

    Added interfaces for expected data formats

---
+++ cloudapp/src/app/userAddressInfo.ts
@@ -0,0 +1,10 @@
+/* Defines the UserAddressInfo entity */
+export interface UserAddressInfo {
+    "id": number,
+    "name": string,
+    "homeAddress": string,
+    "workAddress": string,
+    "alternativeAddress": string,
+    "desiredAddress":string,
+    "checked": boolean
+}
