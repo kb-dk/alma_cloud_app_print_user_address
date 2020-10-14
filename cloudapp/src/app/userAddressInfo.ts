e13783c92aec4606f861b46e25dd95e88bfbdd47
zahra <zahramousavidk@gmail.com>
2020-10-14 00:57:33 -0700

    Refined json structure

--- cloudapp/src/app/userAddressInfo.ts
+++ cloudapp/src/app/userAddressInfo.ts
@@ -1,10 +1,10 @@
 /* Defines the UserAddressInfo entity */
 export interface UserAddressInfo {
-    "id": number,
-    "name": string,
-    "homeAddress": string,
-    "workAddress": string,
-    "alternativeAddress": string,
-    "desiredAddress":string,
-    "checked": boolean
+    id: number,
+    name: string,
+    homeAddress: string,
+    workAddress: string,
+    alternativeAddress: string,
+    desiredAddress:string,
+    checked: boolean
 }
