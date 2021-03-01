c5bd8c09fe23f4a37594f79b7237e255cae6b010
zahra <zahramousavidk@gmail.com>
2021-03-01 06:54:46 -0800

    Add partner service

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import {BehaviorSubject, combineLatest, EMPTY, Subject, Subscription} from 'rxjs';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from '../user.service';
+import {PartnerService} from '../partner.service';
 import {
     CloudAppEventsService,
     CloudAppRestService,
@@ -19,35 +20,75 @@ import {catchError, concatMap, map, tap, toArray} from "rxjs/operators";
 
 export class MainComponent implements OnInit, OnDestroy {
 
-    numRecordsToPrint: number = 0;
-    pageLoaded: boolean = false;
+    numUsersToPrint: number = 0;
+    numPartnersToPrint: number = 0;
     logoUrl: string = '';
     printLogo: boolean = true;
     errorMsg: string = '';
+    selectedTab: string = "0";
+    partnersReady: boolean = false;
+    usersReady: boolean = false;
 
     private currentUserActions;
+    private currentPartnerActions;
     private pageLoadSubscription: Subscription;
     private pageMetadataSubscription: Subscription;
     private pageLoadedSubject = new Subject<Entity[]>();
 
+
+    partnerAction$ = this.pageLoadedSubject.asObservable().pipe(
+        concatMap(entities => this.partnerService.partners$(entities)),
+        tap(currentPartnerAction => this.currentPartnerActions = currentPartnerAction),
+        tap(result => console.log('result:',result)),
+        tap(() => this.partnersReady = true),
+    );
+
     pageLoadedAction$ = this.pageLoadedSubject.asObservable().pipe(
         concatMap(entities => this.userService.users$(entities)),
         tap(currentUserAction => this.currentUserActions = currentUserAction),
-        tap(() => this.pageLoaded = true),
+        tap(() => this.usersReady = true),
     );
 
     private userToggledSubject = new BehaviorSubject<{ id: number, checked: boolean }>({id: -1, checked: false});
     userToggledAction$ = this.userToggledSubject.asObservable();
 
-    private addressSelectedSubject = new BehaviorSubject<{ id: number, value: string }>({id: -1, value: ''});
-    addressSelectedAction$ = this.addressSelectedSubject.asObservable();
+    private partnerToggledSubject = new BehaviorSubject<{ id: number, checked: boolean }>({id: -1, checked: false});
+    partnerToggledAction$ = this.partnerToggledSubject.asObservable();
+
+    private userAddressSelectedSubject = new BehaviorSubject<{ id: number, value: string }>({id: -1, value: ''});
+    userAddressSelectedAction$ = this.userAddressSelectedSubject.asObservable();
+
+    private partnerAddressSelectedSubject = new BehaviorSubject<{ id: number, value: string }>({id: -1, value: ''});
+    partnerAddressSelectedAction$ = this.partnerAddressSelectedSubject.asObservable();
+
+    partnerActions$ = combineLatest([
+        this.partnerAction$,
+        this.partnerToggledAction$,
+        this.partnerAddressSelectedAction$
+    ])
+        .pipe(
+            map(([partners, selectedPartner, selectedAddressType]) =>
+                    partners.map((partner, requestIndex) => ({
+                        ...partner,
+                        selectedAddress: partner.id === selectedAddressType.id ? selectedAddressType.value : this.currentPartnerActions[requestIndex].selectedAddress,
+                        checked: partner.id === selectedPartner.id ? selectedPartner.checked : this.currentPartnerActions[requestIndex].checked,
+                    }) as User),
+                toArray(),
+            ),
+            tap(currentPartnerActions => this.currentPartnerActions = currentPartnerActions),
+            catchError(error => {
+                this.errorMsg = error.message;
+                return EMPTY;
+            }),
+        );
 
     userActions$ = combineLatest([
         this.pageLoadedAction$,
         this.userToggledAction$,
-        this.addressSelectedAction$
+        this.userAddressSelectedAction$
     ])
         .pipe(
+            // tap(([users, selectedUser, selectedAddressType]) => console.log([users, selectedUser, selectedAddressType])),
             map(([users, selectedUser, selectedAddressType]) =>
                     users.map((user, requestIndex) => ({
                         ...user,
@@ -56,6 +97,7 @@ export class MainComponent implements OnInit, OnDestroy {
                     }) as User),
                 toArray(),
             ),
+            tap(currentUserActions => console.log(currentUserActions)),
             tap(currentUserActions => this.currentUserActions = currentUserActions),
             catchError(error => {
                 this.errorMsg = error.message;
@@ -66,7 +108,9 @@ export class MainComponent implements OnInit, OnDestroy {
     constructor(private restService: CloudAppRestService,
                 private configService: CloudAppConfigService,
                 private eventsService: CloudAppEventsService,
-                private userService: UserService) {
+                private userService: UserService,
+                private partnerService: PartnerService
+    ) {
     }
 
     ngOnInit(): void {
@@ -74,7 +118,7 @@ export class MainComponent implements OnInit, OnDestroy {
         this.pageLoadSubscription = this.eventsService.onPageLoad(this.onPageLoad);
 
         this.configService.get().subscribe(
-            config => this.logoUrl = config.logo,
+            config => this.logoUrl = config.user.logo,
             err => console.log(err.message));
     }
 
@@ -84,8 +128,10 @@ export class MainComponent implements OnInit, OnDestroy {
     }
 
     onPageLoad = (pageInfo: PageInfo) => {
+        console.log("step 1:", pageInfo);
         this.onClear();
-        this.pageLoaded = false;
+        this.partnersReady = false;
+        this.usersReady = false;
         this.pageLoadedSubject.next(pageInfo.entities);
     };
 
@@ -94,28 +140,47 @@ export class MainComponent implements OnInit, OnDestroy {
     };
 
     onClear = () => {
-        this.numRecordsToPrint = 0;
-        this.addressSelectedSubject.next({id: -1, value: ''});
+        this.numUsersToPrint = 0;
+        this.numPartnersToPrint = 0;
+        this.userAddressSelectedSubject.next({id: -1, value: ''});
         this.userToggledSubject.next({id: -1, checked: false});
         if (typeof this.currentUserActions !== 'undefined') {
             this.currentUserActions.map(user => {
                 user.checked = false;
             });
         }
+        if (typeof this.currentPartnerActions !== 'undefined') {
+            this.currentPartnerActions.map(user => {
+                user.checked = false;
+            });
+        }
     };
 
     onUserToggled = (e) => {
-        this.numRecordsToPrint = (e.checked) ? this.numRecordsToPrint + 1 : this.numRecordsToPrint - 1;
-        this.userToggledSubject.next({id: e.source.value, checked: e.checked});
+        this.numUsersToPrint = (e.checked) ? this.numUsersToPrint + 1 : this.numUsersToPrint - 1;
+        let user, id;
+        [user, id] = e.source.value.split('_');
+        this.userToggledSubject.next({id: +id, checked: e.checked});
+    };
+
+    onPartnerToggled = (e) => {
+        this.numPartnersToPrint = (e.checked) ? this.numPartnersToPrint + 1 : this.numPartnersToPrint - 1;
+        let partner, id;
+        [partner, id] = e.source.value.split('_');
+        this.partnerToggledSubject.next({id: +id, checked: e.checked});
     };
 
     onAddressSelected = (e) => {
-        let selectedId, selectedAddressType;
-        [selectedId, selectedAddressType] = e.source.value.split('_');
-        this.addressSelectedSubject.next({id: +selectedId, value: selectedAddressType});
+        let selectedId, selectedAddressType, userOrPartner;
+        [userOrPartner, selectedId, selectedAddressType] = e.source.value.split('_');
+        if (userOrPartner === 'user'){
+            this.userAddressSelectedSubject.next({id: +selectedId, value: selectedAddressType});
+        } else {
+            this.partnerAddressSelectedSubject.next({id: +selectedId, value: selectedAddressType});
+        }
     };
 
-    onPrint = () => {
+    onUserPrint = () => {
         let innerHtml: string = "";
 
         this.currentUserActions.map(user => {
@@ -124,7 +189,7 @@ export class MainComponent implements OnInit, OnDestroy {
                 innerHtml = innerHtml.concat(
                     `<div class='pageBreak'>
                       ${logo}  
-                      <p style="position: fixed; top:2cm; width:9cm;">${user.name}<br/>
+                      <p style="position: relative; top:2cm; width:9cm;">${user.name}<br/>
                       ${user.addresses.find(address => address.type === user.selectedAddress).address}</p>
                   </div>`);
             }
@@ -136,8 +201,36 @@ export class MainComponent implements OnInit, OnDestroy {
                                ${innerHtml}
                            </body>
                        </html>`;
+        this.printContent(content);
+    };
 
+    onPartnerPrint = () => {
+        let innerHtml: string = "";
+
+        this.currentPartnerActions.map(partner => {
+            if (partner.checked) {
+                innerHtml = innerHtml.concat(
+                    `<div class='pageBreak'>  
+                      <p style="position: relative; top:0;">${partner.name}<br/>
+                      ${partner.receivers_addresses.find(address => address.type === partner.selectedAddress).address}</p>
+                      <p "position: relative; bottom:0;">${partner.senders_address}</p>
+                  </div>`);
+            }
+        });
+
+        let content = `<html>
+                       <style>@media print {.hidden-print {display: none !important;}} div.pageBreak{page-break-after: always}@page {margin-top: 0;margin-bottom: 0;margin-left: 0;margin-right: 0; width: 10cm;}</style>
+                           <body onload='window.print();' style="font-size:80%; font-family: sans-serif; font-weight:600; margin: 0;">
+                               ${innerHtml}
+                           </body>
+                       </html>`;
+        this.printContent(content);
+
+    };
+
+    printContent = (content) => {
         let win = window.open('', '', 'left=0,top=0,width=552,height=477,toolbar=0,scrollbars=0,status =0');
+        // console.log(content);
         win.document.write(content);
         win.document.close();
     };
