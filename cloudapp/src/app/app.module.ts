33b3d72721e8bf609f09c2c761ee18332e3d0430
zahra <zahramousavidk@gmail.com>
2021-03-04 01:27:52 -0800

    Added FormsModule to app

--- cloudapp/src/app/app.module.ts
+++ cloudapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -31,6 +32,7 @@ export function getToastrModule() {
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     getTranslateModule(),
     getToastrModule()
   ],
